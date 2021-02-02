8435183d5ea6c8eee451b63da45343451acf4376
hmk0006 <hmk0006@mix.wvu.edu>
2021-02-02 18:35:20 -0500

    lab2

---
+++ index.php
@@ -0,0 +1,60 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+    
+    <div class = "about">
+        <h1>About Us
+            <p>asdfghjkl</p>
+        </h1>
+    </div>
+    <div class="row">
+        <div class="column">
+            <dic class="card">
+                <img src="images/1.jpg">
+                <div class="container">
+                    <h2>CEO
+                        <p class="c1">Patrick
+                            <p>PStar@mix.wvu.edu</p>
+                        </p>
+                    </h2>
+                </div>
+            </dic>
+        </div>
+        <div class="column">
+            <dic class="card">
+                <img src="images/2.jpg">
+                <div class="container">
+                    <h2>CEO
+                        <p class="c1">Sad Cat
+                            <p>Cat123@mix.wvu.edu</p>
+                        </p>
+                    </h2>
+                </div>
+            </dic>
+        </div>
+        <div class="column">
+            <dic class="card">
+                <img src="images/3.jpg">
+                <div class="container">
+                    <h2>CEO
+                        <p class="c1">Confused
+                            <p>AlwaysConfused@mix.wvu.edu</p>
+                        </p>
+                    </h2>
+                </div>
+            </dic>
+        </div>
+        <div class="column">
+            <dic class="card">
+                <img src="images/4.jpg">
+                <div class="container">
+                    <h2>CEO
+                        <p class="c1">Rip Off Shrek
+                            <p>ShrekFake@mix.wvu.edu</p>
+                        </p>
+                    </h2>
+                </div>
+            </dic>
+        </div>
+   
+    </div>
+</main>
