d8818dd4a161d19c0af52b010014656f6b7e0fa0
hmk0006 <hmk0006@mix.wvu.edu>
2021-02-02 19:40:59 -0500

    Lab2_Startup

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
     <link rel="stylesheet" href="css/about.css">
     
     <div class = "about">
-        <h1>About Us
-            <p>asdfghjkl</p>
+        <h1>Cryptocurrency Shop
+        <p>-----------------------</p>
+            <p>Welcome to our site!</p>
         </h1>
+        <h2>
+        <p>These four cryptocurrencies are on the rise. They need your help to go to the moon!</p>
+        <p>Click to purchase, find more information, or follow on social media!</p>
+        </h2>
+
     </div>
     <div class="row">
         <div class="column">
@@ -12,8 +18,8 @@
                 <img src="images/1.jpg">
                 <div class="container">
                     <h2>CEO
-                        <p class="c1">Patrick
-                            <p>PStar@mix.wvu.edu</p>
+                        <p class="c1">Bitcoin
+                            <p>Bitcoin@mix.wvu.edu</p>
                         </p>
                     </h2>
                 </div>
@@ -24,8 +30,8 @@
                 <img src="images/2.jpg">
                 <div class="container">
                     <h2>CEO
-                        <p class="c1">Sad Cat
-                            <p>Cat123@mix.wvu.edu</p>
+                        <p class="c1">Dogecoin
+                            <p>Dogecoin@mix.wvu.edu</p>
                         </p>
                     </h2>
                 </div>
@@ -36,8 +42,8 @@
                 <img src="images/3.jpg">
                 <div class="container">
                     <h2>CEO
-                        <p class="c1">Confused
-                            <p>AlwaysConfused@mix.wvu.edu</p>
+                        <p class="c1">Litecoin
+                            <p>Litecoin@mix.wvu.edu</p>
                         </p>
                     </h2>
                 </div>
@@ -48,8 +54,8 @@
                 <img src="images/4.jpg">
                 <div class="container">
                     <h2>CEO
-                        <p class="c1">Rip Off Shrek
-                            <p>ShrekFake@mix.wvu.edu</p>
+                        <p class="c1">Ethereum
+                            <p>Ethereum@mix.wvu.edu</p>
                         </p>
                     </h2>
                 </div>
